eb3b6c2d80711c95a632debdb2fc116e5dd25275
abhayMore <49201569+abhayMore@users.noreply.github.com>
2020-01-05 15:51:49 +0530

    Added extra commands
    commands nginx, nodejs, , net-tools and repository for brackets.io have been added. Removed unnecessary lines.

--- StarterScript.sh
+++ StarterScript.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo "Welcome to daily routine diagnosis";
-echo "System is updating and installing the required softwares";
-echo "Please wait till the process completes";
 sudo apt-get update
 sudo add-apt-repository universe 
 sudo add-apt-repository multiverse
+sudo add-apt-repository ppa:webupd8team/brackets -yes
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install guake -y
@@ -12,6 +10,9 @@ sudo apt-get install vlc -y
 sudo apt-get install vim -y
 sudo apt install chromium-browser -y
 sudo apt-get install git -y
-git config --global user.email abhay7more@gmail.com
-git config --global user.name abhayMore
-echo ".......The process ended and you are updated to your requirements.......";
+git config --global user.email (email id)
+git config --global user.name (username)
+sudo apt-install nginx -y
+sudo apt-install nodejs -y
+sudo apt-install net-tools -y
+sudo apt-get install brackets -y
